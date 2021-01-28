55e34f2ae95e9deec369e2ff8ec2d602fe75dadd
Siddharth Pandey <siddharth25pandey@gmail.com>
2021-01-28 15:41:00 +0530

    Uploaded in Django

---
+++ hospital_appointment_and_information_system/hospital_appointment_and_information_system/urls.py
@@ -0,0 +1,30 @@
+"""hospital_appointment_and_information_system URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from hospital import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home_view,name=''),
+    path('about/',views.about_view,name='about.html'),
+    path('login/',views.login_view,name='login.html'),
+    path('news/',views.news_view,name='news.html'),
+    path('signup/',views.signup_view,name='signup.html'),
+    path('contact_us/',views.contact_us_view,name='contact_us.html'),
+    #path('hospital/', include('hospital.urls')),
+    
+]
