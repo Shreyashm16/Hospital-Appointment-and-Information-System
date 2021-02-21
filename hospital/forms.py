f75a3ca992feab345fbcacfecac64e23cab609c5
prichoms <bajpaipriyam90@gmail.com>
2021-02-21 14:56:46 +0530

    form

--- hospital/forms.py
+++ hospital/forms.py
@@ -4,6 +4,9 @@ from . import models
 
 
 class ContactusForm(forms.Form):
-    Name = forms.CharField(max_length=30)
-    Email = forms.EmailField()
-    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+    Name = forms.CharField(max_length=30,label="",widget=forms.TextInput(attrs={'placeholder': 'NAME'}))
+    Name.widget.attrs.update({'class' : 'app-form-control'})
+    Email = forms.EmailField(label="",widget=forms.TextInput(attrs={'placeholder': 'EMAIL'}))
+    Email.widget.attrs.update({'class' : 'app-form-control'})
+    Message = forms.CharField(max_length=500,label="",widget=forms.TextInput(attrs={'placeholder': 'MESSAGE'}))
+    Message.widget.attrs.update({'class' : 'app-form-control'})
