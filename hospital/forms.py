06f3f2813e36ea63e4a7302e7756a96a81879ddc
prichoms <bajpaipriyam90@gmail.com>
2021-04-14 00:05:46 +0530

    registerform ui

--- hospital/forms.py
+++ hospital/forms.py
@@ -23,21 +23,47 @@ class ContactusForm(forms.Form):
     Message.widget.attrs.update({'class' : 'app-form-control'})
 
 class DoctorRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    firstname = forms.CharField()
-    lastname = forms.CharField()
-    age = forms.IntegerField()
-    dob = forms.DateField(widget=SelectDateWidget(years=range(1960, 2021)))
-    address = forms.CharField()
-    city = forms.CharField()
-    country = forms.CharField(initial="india")
-    postalcode = forms.IntegerField()
-    image = forms.ImageField()
-    department= forms.MultipleChoiceField(choices=dep)
-    password1 = forms.CharField(label='Enter password', 
-                                widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', 
-                                widget=forms.PasswordInput)
+    username = forms.CharField(required=True,label="",widget=forms.TextInput(attrs={'placeholder': 'USERNAME'}))
+    username.widget.attrs.update({'class' : 'app-form-control'})
+    
+    email = forms.EmailField(required=True,label="",widget=forms.TextInput(attrs={'placeholder': 'EMAIL'}))
+    email.widget.attrs.update({'class' : 'app-form-control'})
+    
+    firstname = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'FIRSTNAME'}))
+    firstname.widget.attrs.update({'class' : 'app-form-control'})
+    
+    lastname = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'LASTNAME'}))
+    lastname.widget.attrs.update({'class' : 'app-form-control'})
+    
+    age = forms.IntegerField(label="",widget=forms.TextInput(attrs={'placeholder': 'AGE'}))
+    age.widget.attrs.update({'class' : 'app-form-control'})
+    
+    dob = forms.DateField(label="",widget=SelectDateWidget(years=range(1960, 2021)))
+    dob.widget.attrs.update({'class' : 'app-form-control-date'})
+    
+    address = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'ADDRESS'}))
+    address.widget.attrs.update({'class' : 'app-form-control'})
+    
+    city = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'CITY'}))
+    city.widget.attrs.update({'class' : 'app-form-control'})
+    
+    country = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'COUNTRY'}))
+    country.widget.attrs.update({'class' : 'app-form-control'})
+    
+    postalcode = forms.IntegerField(label="",widget=forms.TextInput(attrs={'placeholder': 'POSTAL CODE'}))
+    postalcode.widget.attrs.update({'class' : 'app-form-control'})
+    
+    image = forms.ImageField(label="",widget=forms.TextInput(attrs={'placeholder': 'NAME'}))
+    image.widget.attrs.update({'class' : 'app-form-control'})
+    
+    department = forms.CharField(label="",widget = forms.Select(choices=dep))
+    department.widget.attrs.update({'class' : 'app-form-control'})
+
+    password1 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'PASSWORD'}))
+    password1.widget.attrs.update({'class' : 'app-form-control'})
+    
+    password2 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'RE-CONFIRM'}))
+    password2.widget.attrs.update({'class' : 'app-form-control'})
 
     class Meta:
         model = User
@@ -61,20 +87,44 @@ class DoctorUpdateForm(forms.ModelForm):
 
 
 class AdminRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    firstname = forms.CharField()
-    lastname = forms.CharField()
-    age = forms.IntegerField()
-    dob = forms.DateField(widget=SelectDateWidget(years=range(1960, 2021)))
-    address = forms.CharField()
-    city = forms.CharField()
-    country = forms.CharField()
-    postalcode = forms.IntegerField()
-    image = forms.ImageField()
-    password1 = forms.CharField(label='Enter password', 
-                                widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', 
-                                widget=forms.PasswordInput)
+    username = forms.CharField(required=True,label="",widget=forms.TextInput(attrs={'placeholder': 'USERNAME'}))
+    username.widget.attrs.update({'class' : 'app-form-control'})
+    
+    email = forms.EmailField(required=True,label="",widget=forms.TextInput(attrs={'placeholder': 'EMAIL'}))
+    email.widget.attrs.update({'class' : 'app-form-control'})
+    
+    firstname = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'FIRSTNAME'}))
+    firstname.widget.attrs.update({'class' : 'app-form-control'})
+    
+    lastname = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'LASTNAME'}))
+    lastname.widget.attrs.update({'class' : 'app-form-control'})
+    
+    age = forms.IntegerField(label="",widget=forms.TextInput(attrs={'placeholder': 'AGE'}))
+    age.widget.attrs.update({'class' : 'app-form-control'})
+    
+    dob = forms.DateField(label="",widget=SelectDateWidget(years=range(1960, 2021)))
+    dob.widget.attrs.update({'class' : 'app-form-control-date'})
+    
+    address = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'ADDRESS'}))
+    address.widget.attrs.update({'class' : 'app-form-control'})
+    
+    city = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'CITY'}))
+    city.widget.attrs.update({'class' : 'app-form-control'})
+    
+    country = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'COUNTRY'}))
+    country.widget.attrs.update({'class' : 'app-form-control'})
+    
+    postalcode = forms.IntegerField(label="",widget=forms.TextInput(attrs={'placeholder': 'POSTAL CODE'}))
+    postalcode.widget.attrs.update({'class' : 'app-form-control'})
+    
+    image = forms.ImageField(label="",widget=forms.TextInput(attrs={'placeholder': 'NAME'}))
+    image.widget.attrs.update({'class' : 'app-form-control'})
+    
+    password1 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'PASSWORD'}))
+    password1.widget.attrs.update({'class' : 'app-form-control'})
+    
+    password2 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'RE-CONFIRM'}))
+    password2.widget.attrs.update({'class' : 'app-form-control'})
 
     class Meta:
         model = User
