e12009b0351c8df196cf7f243b5829ed96b104d5
Shreyash Mishra <shreyashm1601@gmail.com>
2021-04-15 09:47:04 +0530

    appointment form v1.5

--- hospital/forms.py
+++ hospital/forms.py
@@ -218,11 +218,11 @@ class PatientUpdateForm(forms.ModelForm):
 class PatientAppointmentForm(forms.ModelForm):
     doctorId=forms.ModelChoiceField(queryset=Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="id",label="Doctor")
     #doctorId=forms.IntegerField()
-    #description = forms.CharField(widget=forms.Textarea)
-    #appointmentDate = forms.DateField(widget=SelectDateWidget(years=range(2021,2024)))
+    description = forms.CharField(widget=forms.Textarea)
+    appointmentDate = forms.DateField(widget=SelectDateWidget(years=range(2021,2024)))
     class Meta:
         model=Appointment
-        fields=['description','status']
+        fields=['doctorId','description','appointmentDate']
 
 
 class AdminAppointmentForm(forms.ModelForm):
