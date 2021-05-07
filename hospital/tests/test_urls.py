40d9f84935d9838cc595a3ac914ccd498720e35d
Shreyash Mishra <shreyashm1601@gmail.com>
2021-05-07 07:46:56 +0530

    unit test views

--- hospital/tests/test_urls.py
+++ hospital/tests/test_urls.py
@@ -7,16 +7,20 @@ class TestUrls(SimpleTestCase):
 
     def test_urls_is_resolved(self):
         url = reverse('')
+        #print(resolve(url))
         self.assertEquals(resolve(url).func,home_view)
     
     def test_urls_is_resolved_bookapp(self):
         url = reverse('bookapp.html')
+        #print(resolve(url))
         self.assertEquals(resolve(url).func,bookapp_view)
     
     def test_urls_is_resolved_calladoc(self):
         url = reverse('calladoc.html')
+        #print(resolve(url))
         self.assertEquals(resolve(url).func,calladoc_view)
     
     def test_urls_is_resolved_feedback(self):
         url = reverse('feedback.html')
+        #print(resolve(url))
         self.assertEquals(resolve(url).func,feedback_view)
---
+++ hospital/tests/test_views.py
@@ -0,0 +1,69 @@
+from django.test import TestCase,Client
+from django.urls import reverse
+from hospital.models import Patient,Admin,Doctor,User,Appointment
+from hospital.forms import AdminAppointmentForm
+import json
+from django.utils import timezone
+from datetime import date,timedelta,time
+
+class TestViews(TestCase):
+    nu = User(username='username',email='email@gmail.com',password='password1')
+    doc = Doctor(user=nu,firstname='firstname',
+                        lastname='lastname',
+                        department='department',
+                        dob='12/12/12',
+                        address='address',
+                        city='city',
+                        country='country',
+                        postalcode=12345)
+    pat = Patient(user=nu,firstname='firstname',
+                        lastname='lastname',
+                        dob='12/12/12',
+                        address='address',
+                        city='city',
+                        country='country',
+                        postalcode=12345)
+    
+    def test_GET_home_view(self):
+        client = Client()
+        response = client.get(reverse(''))
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'hospital/Home/home.html')
+
+    def test_GET_login_view(self):
+        client = Client()
+        response = client.get(reverse('login.html'))
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'hospital/Home/login.html')
+
+    def test_GET_login_pat_view(self):
+        client = Client()
+        response = client.get(reverse('login_pat.html'))
+
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'hospital/Patient/login_pat.html')
+    
+    def test_GET_bookapp_adm_view(self):
+        client = Client()
+        appointmentForm = AdminAppointmentForm()
+
+        #response = client.get(reverse('bookapp_adm.html',args=[appointmentForm]))
+        response = client.get(reverse('bookapp_adm.html'))
+        self.assertEquals(response.status_code,302)
+        #self.assertTemplateUsed(response,'hospital/Admin/bookapp_adm.html')
+    
+    def test_POST_bookapp_adm_view(self):
+        client = Client()
+        dt = timezone.now().date()
+        tm = timezone.now().time()
+        response = client.post(reverse('bookapp_adm.html'),{
+            'description': "testing bookapp_adm",
+            'calldate': dt,
+            "calltime": tm,
+            'doctor': self.doc,
+            'patient': self.pat
+        })
+        self.assertEquals(response.status_code,302)
+ 
